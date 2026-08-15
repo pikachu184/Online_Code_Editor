07522347a461ccac8bbe4561b298a1d76f3f4ab0
Khushi Jain <khushijainjain467@gmail.com>
2026-08-15 16:39:14 +0530

    this is the proper working code with implament docker and run basic code without error

--- OnLine-code-editor/src/test/java/com/micro/onlinecodeeditor/OnLineCodeEditorApplicationTests.java
+++ OnLine-code-editor/src/test/java/com/micro/onlinecodeeditor/OnLineCodeEditorApplicationTests.java
@@ -3,8 +3,10 @@ package com.micro.onlinecodeeditor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+class OnLineCodeEditorApplicationTests {
+
     @Test
     void contextLoads() {
     }
-
 }
